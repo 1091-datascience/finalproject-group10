299b62a22322751819d9999bea123a0d1c331562
Zovjsra <4703michael@gmail.com>
2021-01-11 13:47:41 +0800

    先把data丟上去

---
+++ code/vis/ui.r
@@ -0,0 +1,27 @@
+library(ggvis)
+library(shiny)
+shinyUI(pageWithSidebar(
+  div(),
+  sidebarPanel(
+    selectInput("vert", "Vertical:",
+            c("PC1" = 1,
+              "PC2" = 2,
+              "PC3" = 3,
+              'PC4' = 4)),
+    selectInput("horz", "Horizonal:",
+              c("PC1" = 1,
+              "PC2" = 2,
+              "PC3" = 3,
+              'PC4' = 4)),
+  ),
+  mainPanel(
+    plotOutput("plot_ui"),
+    titlePanel('PCA.rotation'),
+    tableOutput("table2"),
+    titlePanel('Summary'),
+    tableOutput('table3'),
+    titlePanel('Iris'),
+    dataTableOutput("table1"),
+
+    )
+))
